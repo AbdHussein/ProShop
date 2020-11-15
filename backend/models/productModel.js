7c6835e68615adbf21702d828a4dd6a38ac479b8
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-11-15 20:02:20 +0200

    Error Handling middlewares Implemented

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -34,6 +34,6 @@ const productSchema = mongoose.Schema(
   }
 );
 
-const Product = mongoose.Model('Product', productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 export default Product;
