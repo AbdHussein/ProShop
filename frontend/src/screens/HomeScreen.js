953be99605968c4d435a75f0cf9cdd701b751768
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-11-22 14:30:14 +0200

    Redux Implemented

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
 import Product from '../components/Product';
+import { listProduct } from '../actions/productActions';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProduct());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products &&
-          products.map((product) => (
-            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-              {/* <h3>{product.name}</h3> */}
-              <Product key={product._id} product={product} />
-            </Col>
-          ))}
-      </Row>
+      {loading ? (
+        <h2>Loading</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products &&
+            products.map((product) => (
+              <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                {/* <h3>{product.name}</h3> */}
+                <Product key={product._id} product={product} />
+              </Col>
+            ))}
+        </Row>
+      )}
     </>
   );
 };
